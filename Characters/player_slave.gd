6cb4677b118529f5811bf1533b5f0584d1727ef3
Kursat <draknes.gamin70@gmail.com>
2023-04-01 00:54:25 +0300

    Added platforms and movement

--- Characters/player_slave.gd
+++ Characters/player_slave.gd
@@ -1,13 +1,29 @@
 extends CharacterBody2D
-@export var mov_speed = 100
 
-func _physics_process(_delta):
+@export var SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
 	
-	#get input direction
-	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("jump")
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
 		
-	)
-	print(input_direction)
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	move_and_slide()
+	
