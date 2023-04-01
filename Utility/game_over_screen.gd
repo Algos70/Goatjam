f93c6a891b569f1622ed8c75ec1b85f1a3bb4f68
Kursat <draknes.gamin70@gmail.com>
2023-04-01 15:18:25 +0300

    GUI and game over screen done

--- Utility/game_over_screen.gd
+++ Utility/game_over_screen.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 
 func _on_restart_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Levels/game_level.tscn")
 
 
 func _on_quit_button_pressed():
