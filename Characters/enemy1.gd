15d34a1b18e9ca55bec0595ef340d58555d90177
electi <barankircaa@gmail.com>
2023-04-01 15:19:02 +0300

    added block mechanism

--- Characters/enemy1.gd
+++ Characters/enemy1.gd
@@ -8,16 +8,16 @@ var turnBackTimer = 0
 var isFacingLeft = true
 var canInteract = false
 var canDefend = true
-var canAttack = true
+
 var defenceCooldown = 0
-var attackCooldown = 0
+
 var state = "idle"
 var landed = false
 var isOnEdge = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var rng = RandomNumberGenerator.new()
 
 
-@onready var attackRange = $"attack_range"
 @onready var detectionRange = $"detectionRange"
 @onready var player = get_parent().get_node("PlayerSlave") #change player name
 
@@ -26,11 +26,6 @@ func _process(delta):
 		defenceCooldown -= delta
 	else:
 		canDefend = true
-		
-	if attackCooldown > 0:
-		attackCooldown -= delta
-	else:
-		canAttack = true
 		
 	if turnBackTimer > 0:
 		turnBackTimer -= delta
@@ -52,54 +47,38 @@ func _process(delta):
 		else:
 			velocity.x = IDLE_SPEED * (-1 if isFacingLeft else 1) 
 	elif state == "detected":
-		if (canInteract):
-			attack()
+		if player.position.x < position.x:
+			velocity.x = -CHASE_SPEED
+			isFacingLeft = true
 		else:
-			if player.position.x < position.x:
-				velocity.x = -CHASE_SPEED
-				isFacingLeft = true
-			else:
-				velocity.x = CHASE_SPEED
-				isFacingLeft = false	
-		# Move the enemy
+			velocity.x = CHASE_SPEED
+			isFacingLeft = false	
+	# Move the enemy
 	move_and_slide()		
 	
 	# Flip sprite to face direction of movement
 	if velocity.x != 0:
 		get_node("Sprite2D").flip_h = isFacingLeft
 	
-func _on_player_attack():
-	if canDefend:
-		defend()
+func on_player_attack():
+	var choice = rng.randi_range(0, 1)
+	if canDefend and choice == 0:
 		defenceCooldown = 3
-
-func defend():
-	pass
-	
-func attack():
-	if canAttack:
-		pass
-	attackCooldown = 1
-	pass
+		canDefend = false
+		return true
+	return false
+		
 
 func _on_hurt_box_hurt(damage):
-	print(hp)
-	if Input.is_action_pressed("attack"):
-		hp -= damage
-	if hp <= 0:
-		queue_free() #if hp 0 or lower delete the enemy from the game
-		
-	move_and_slide()
-
-
-func _on_attack_range_body_entered(body):
-	if body.name == player.name:
-		canInteract = true
-
-
-func _on_attack_range_body_exited(body):
-	if body.name == player.name:
-		canInteract = false
+	var protected = on_player_attack()
+	if (not protected):
+		print(hp)
+		if Input.is_action_pressed("attack"):
+			hp -= damage
+		if hp <= 0:
+			queue_free() #if hp 0 or lower delete the enemy from the game
+			
+		move_and_slide()
 
 
 func _on_detection_range_body_entered(body):
