159e01d271c4e46725f1082903013899077e70e5
Kursat <draknes.gamin70@gmail.com>
2023-04-02 12:25:06 +0300

    Control UI update

--- UI/control_page.gd
+++ UI/control_page.gd
@@ -1,5 +1,8 @@
 extends Control
 
+@onready var click = $Back/Click
+@onready var pressed = $Back/Pressed
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,4 +15,9 @@ func _process(delta):
 
 
 func _on_back_pressed():
+	pressed.play()
 	get_tree().change_scene_to_file("res://UI/welcome_page.tscn")
+
+
+func _on_back_mouse_entered():
+	click.play()
